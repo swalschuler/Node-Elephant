c72a396817f86009fed991cceee0b54bbb329bd2
Yi Lu <luyi326@gmail.com>
2015-04-23 04:18:49 -0700

    Revert "Commented out Filter stuff, added running average but will not use,"
    This reverts commit 6bbf6fc040bb865344e7d432f0bc7f050d927fad.

--- Node-Elephant/Node-Elephant.cydsn/codegentemp/Node-Elephant.ctl
+++ Node-Elephant/Node-Elephant.cydsn/codegentemp/Node-Elephant.ctl
@@ -1,6 +1,6 @@
 -- ======================================================================
 -- Node-Elephant.ctl generated from Node-Elephant
--- 12/11/2014 at 03:52
+-- 12/10/2014 at 00:23
 -- This file is auto generated. ANY EDITS YOU MAKE MAY BE LOST WHEN THIS FILE IS REGENERATED!!!
 -- ======================================================================
 
--- Node-Elephant/Node-Elephant.cydsn/main.c
+++ Node-Elephant/Node-Elephant.cydsn/main.c
@@ -15,12 +15,10 @@
 #include <stdio.h>
 #include "calibrate.h"
 
-//#define DMA_BYTES_PER_BURST     12
-//#define DMA_REQUEST_PER_BURST   1
-//#define DMA_SRC_ADDRESS        CYDEV_PERIPH_BASE
-//#define DMA_DEST_ADDRESS       CYDEV_PERIPH_BASE
-
-#define BUFF_LENGTH     300
+#define DMA_BYTES_PER_BURST     12
+#define DMA_REQUEST_PER_BURST   1
+#define DMA_SRC_ADDRESS        CYDEV_PERIPH_BASE
+#define DMA_DEST_ADDRESS       CYDEV_PERIPH_BASE
 
 void calAll(void);          //calibrate all sensors
 
@@ -39,94 +37,82 @@ int main()
     Filter_Start();
     CyGlobalIntEnable;          //enable global interrupts 
     
-    uint16 throttle;
-    uint16 throttle1;            //variables for sending average to can
-    uint16 throttle2;
-    uint16 brake1;
-    uint16 brake2;
-    uint16 steering;
-    uint16 buffT1[BUFF_LENGTH];
-    uint16 buffT2[BUFF_LENGTH];         //variables for buffering ADC results between interrupts
-    uint16 buffB1[BUFF_LENGTH];
-    uint16 buffB2[BUFF_LENGTH];
-    uint16 buffST[BUFF_LENGTH];
-
-    uint8 i;
-    uint8 j;
-    uint8 buffSize = 0;           //tracks number of conversions 
-    uint16 window = 10;           //average width 
+    uint32 throttle = 0;
+    uint32 throttle1 = 0;            //declared 32 bits for calculation, will cast to 16 bit when pushing to can
+    uint32 throttle2 = 0;
+    uint32 brake1 = 0;
+    uint32 brake2 = 0;
+    uint32 steering = 0;
+    uint32 averager = 0;         //counts number of times adc gets a result and used as divisor for average
     
-    LCD_Position(0,0);
-    LCD_PrintString("Throttle 1 AVG:");
+//    for(;;)
+//    {                  
+//        if(toggle_flag == 1)            //if interrupt fired
+//        {
+//            throttle = (float)(throttle1/averager) + 0.5;           //gets rounded average value over time interval between interrupts
+//            throttle2 =(float)(throttle2/averager) + 0.5;
+//            brake1 = (float)(brake1/averager) + 0.5;
+//            brake2 = (float)(brake2/averager) + 0.5;
+//            steering = (float)(steering/averager) + 0.5;
+//            
+//            LED_Write(~LED_Read());             //visual check is interrupt is running
+//            
+//            LCD_ClearDisplay();
+//            LCD_Position(0,0);              //displaying number of times values were retrieved 
+//            LCD_PrintNumber(averager);
+//            LCD_Position(1,0);              //displaying number of times values were retrieved 
+//            LCD_PrintNumber(throttle1);
+//            //CyDelay(2000);
+//            LCD_Position(0,7);              //displaying number of times values were retrieved 
+//            LCD_PrintNumber(throttle);
+//            averager = 0;
+//            toggle_flag = 0;
+//            throttle = 0;
+//            throttle1 = 0;
+//            throttle2 = 0;
+//            brake1 = 0;
+//            brake2 = 0;
+//            steering = 0;
+//            averager = 0;
+//        }
+//        
+//        if(ADC_SAR_IsEndConversion(ADC_SAR_WAIT_FOR_RESULT))
+//        {
+//            throttle1 = throttle1 + ADC_SAR_GetResult16(0);         //summing values over the time interval between interrupts of each channel
+//            throttle2 = throttle2 + ADC_SAR_GetResult16(1);
+//            brake1 = brake1 + ADC_SAR_GetResult16(2);
+//            brake2 = brake2 + ADC_SAR_GetResult16(3);
+//            steering = steering + ADC_SAR_GetResult16(4);
+////            LCD_Position(1,5);              //displaying number of times values were retrieved 
+////            LCD_PrintNumber(averager);
+//            averager++;
+//            //CyDelay(2000);
+//        }
+//    }
+    
+    uint8 tdChanA;
+    uint8 myChannel;
+    
+    LCD_ClearDisplay();
+    
+    myChannel = DMA_DmaInitialize(DMA_BYTES_PER_BURST, DMA_REQUEST_PER_BURST, HI16(ADC_SAR_COUNT_PTR), HI16(DMA_DEST_ADDRESS));
+    tdChanA = CyDmaTdAllocate();
+    CyDmaTdSetConfiguration(tdChanA, DMA_BYTES_PER_BURST, tdChanA, 0);
+    CyDmaTdSetAddress(tdChanA, LO16((uint32)ADC_SAR_COUNT_PTR), LO16((uint32)Filter_STAGEAH_PTR));
+//    CyDmaTdSetAddress(tdChanA, LO16(DMA_SRC_ADDRESS), LO16(DMA_DEST_ADDRESS));
+    CyDmaChSetInitialTd(myChannel, tdChanA);
+    CyDmaChEnable(myChannel, 1u);
     
     for(;;)
     {
         if(toggle_flag == 1)
         {
-            for(i = window; i < buffSize; i++)           //computes moving average
-            {
-                throttle = 0;
-                throttle1 = 0;            //reset variables for sending average to can
-                throttle2 = 0;
-                brake1 = 0;
-                brake2 = 0;
-                steering = 0;
-                
-                for(j = 0+i; j+i < window+i; j++)         //gets the set to be averaged; size specified by window
-                {
-                    throttle1 += buffT1[i];
-                    throttle2 += buffT2[i];             
-                    brake1 += buffB1[i];
-                    brake2 += buffB2[i];
-                    steering += buffST[i];
-                }
-                
-                throttle1 /= window;            //averages the values of the window set
-                throttle2 /= window;             
-                brake1 /= window;
-                brake2 /= window;
-                steering /= window;
-            }
+            throttle = Filter_Read16(Filter_CHANNEL_A );
             
-            LCD_Position(1,0);
-            LCD_PrintNumber(throttle1);
-        }
-        
-        if(ADC_SAR_IsEndConversion(ADC_SAR_WAIT_FOR_RESULT))  
-        {
-            buffT1[buffSize] = ADC_SAR_GetResult16(0);        //get conversion results in terms of counts for throttle 1 (channel 0)  
-            buffT2[buffSize] = ADC_SAR_GetResult16(1);        //get conversion results in terms of counts for throttle 2 (channel 1)  
-            buffB1[buffSize] = ADC_SAR_GetResult16(2);        //get conversion results in terms of counts for brake 1 (channel 2)  
-            buffB2[buffSize] = ADC_SAR_GetResult16(3);        //get conversion results in terms of counts for brake 2 (channel 3)  
-            buffST[buffSize] = ADC_SAR_GetResult16(4);        //get conversion results in terms of counts for steering (channel 4)  
-            buffSize++;
+            LCD_Position(0,0);
+            LCD_PrintNumber(throttle);
         }
     }
-    
-    
-//    uint8 tdChanA;
-//    uint8 myChannel;
-//    
-//    LCD_ClearDisplay();
-//    
-//    myChannel = DMA_DmaInitialize(DMA_BYTES_PER_BURST, DMA_REQUEST_PER_BURST, HI16(ADC_SAR_COUNT_PTR), HI16(DMA_DEST_ADDRESS));
-//    tdChanA = CyDmaTdAllocate();
-//    CyDmaTdSetConfiguration(tdChanA, DMA_BYTES_PER_BURST, tdChanA, 0);
-//    CyDmaTdSetAddress(tdChanA, LO16((uint32)ADC_SAR_COUNT_PTR), LO16((uint32)Filter_STAGEAH_PTR));
-////    CyDmaTdSetAddress(tdChanA, LO16(DMA_SRC_ADDRESS), LO16(DMA_DEST_ADDRESS));
-//    CyDmaChSetInitialTd(myChannel, tdChanA);
-//    CyDmaChEnable(myChannel, 1u);
-//    
-//    for(;;)
-//    {
-//        if(toggle_flag == 1)
-//        {
-//            throttle = Filter_Read16(Filter_CHANNEL_A );
-//            
-//            LCD_Position(0,0);
-//            LCD_PrintNumber(throttle);
-//        }
-//    }
     
     return 0;
 }
